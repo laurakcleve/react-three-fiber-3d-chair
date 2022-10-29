7e2e76847d3448a521d4791b3dc77ef9d634a94b
Laura <laurakcleve@gmail.com>
2022-10-29 14:44:52 -0600

    Add shadows, change camera

--- src/App.js
+++ src/App.js
@@ -2,12 +2,12 @@ import { Suspense } from 'react'
 import { Chair } from './Chair'
 import { Armchair } from './Armchair'
 import { Canvas } from '@react-three/fiber'
-import { OrbitControls } from '@react-three/drei'
+import { ContactShadows, OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
     <>
-      <Canvas camera={{ fov: 70, position: [4, 1.5, 3.5] }}>
+      <Canvas camera={{ fov: 70, position: [3, 1.5, 4] }}>
         <ambientLight intensity={0.5} />
         <spotLight
           intensity={0.5}
@@ -17,11 +17,18 @@ function App() {
         ></spotLight>
         <Suspense fallback='null'>
           <Chair />
+          <ContactShadows
+            position={[0, -1.8, 0]}
+            opacity={0.5}
+            scale={100}
+            blur={0.4}
+            color='#000000'
+          />
         </Suspense>
         <OrbitControls />
       </Canvas>
 
-      <Canvas camera={{ fov: 70, position: [4, 1.5, 3.5] }}>
+      <Canvas camera={{ fov: 70, position: [3, 2, 4] }}>
         <ambientLight intensity={0.5} />
         <spotLight
           intensity={0.5}
@@ -31,6 +38,13 @@ function App() {
         ></spotLight>
         <Suspense fallback='null'>
           <Armchair />
+          <ContactShadows
+            position={[0, -0.92, 0]}
+            opacity={0.5}
+            scale={100}
+            blur={0.4}
+            color='#000000'
+          />
         </Suspense>
         <OrbitControls />
       </Canvas>
