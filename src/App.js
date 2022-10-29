6cfb01e885288280a4e20ecd32b758413493df5f
Laura <laurakcleve@gmail.com>
2022-10-29 13:36:43 -0600

    Remove react app stuff

--- src/App.js
+++ src/App.js
@@ -1,28 +1,23 @@
 import { Suspense } from 'react'
-import './App.css'
 import { Chair } from './Chair'
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
-    <div className='App'>
-      <header className='App-header'>
-        <Canvas camera={{ fov: 70 }}>
-        <ambientLight intensity={0.5} />
-          <spotLight
-            intensity={0.5}
-            position={[10, 10, 10]}
-            angle={0.15}
-            penumbra={1}
-          ></spotLight>
-          <Suspense fallback='null'>
-            <Chair />
-          </Suspense>
-          <OrbitControls />
-        </Canvas>
-      </header>
-    </div>
+    <Canvas camera={{ fov: 70 }}>
+      <ambientLight intensity={0.5} />
+      <spotLight
+        intensity={0.5}
+        position={[10, 10, 10]}
+        angle={0.15}
+        penumbra={1}
+      ></spotLight>
+      <Suspense fallback='null'>
+        <Chair />
+      </Suspense>
+      <OrbitControls />
+    </Canvas>
   )
 }
 
