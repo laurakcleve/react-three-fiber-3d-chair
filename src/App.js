faf48b2e9eae2ebd972256ddb1c50761cb51378e
Laura <laurakcleve@gmail.com>
2022-10-30 12:09:02 -0600

    Remove armchair, add mouseover highlight

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import { Suspense } from 'react'
 import { Chair } from './Chair'
-import { Armchair } from './Armchair'
 import { Canvas } from '@react-three/fiber'
-import { ContactShadows, OrbitControls } from '@react-three/drei'
+import { ContactShadows, Environment, OrbitControls } from '@react-three/drei'
 
 function App() {
   return (
     <>
       <Canvas camera={{ fov: 70, position: [3, 1.5, 4] }}>
-        <ambientLight intensity={0.5} />
-        <spotLight
-          intensity={0.5}
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-        ></spotLight>
+        <spotLight intensity={1} position={[20, 50, 50]} angle={0.5}></spotLight>
         <Suspense fallback='null'>
           <Chair />
+          <Environment preset='city' />
           <ContactShadows
             position={[0, -1.8, 0]}
-            opacity={0.5}
-            scale={100}
-            blur={0.4}
-            color='#000000'
-          />
-        </Suspense>
-        <OrbitControls />
-      </Canvas>
-
-      <Canvas camera={{ fov: 70, position: [3, 2, 4] }}>
-        <ambientLight intensity={0.5} />
-        <spotLight
-          intensity={0.5}
-          position={[10, 10, 10]}
-          angle={0.15}
-          penumbra={1}
-        ></spotLight>
-        <Suspense fallback='null'>
-          <Armchair />
-          <ContactShadows
-            position={[0, -0.92, 0]}
             opacity={0.5}
             scale={100}
             blur={0.4}
